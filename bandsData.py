bd74a25c8969c7e2d913356bb4a11ffe21cf8f6d
ThisTemba <tembamateke@gmail.com>
2022-08-15 14:00:23 -0400

    Add bandsData

---
+++ bandsData.py
@@ -0,0 +1,44 @@
+bands = {
+    "B0": {
+        "corrFilename": "Rod A 1 kHz.csv",
+        "stateFilename": "State_B0.state",
+        "coupling": "DC"
+    },
+    "B1": {
+        "corrFilename": "Rod A 10 kHz.csv",
+        "stateFilename": "State_B1.state",
+        "coupling": "DC"
+    },
+    "B2": {
+        "corrFilename": "Rod A 10 kHz.csv",
+        "stateFilename": "State_B2.state",
+        "coupling": "DC"
+    },
+    "B3": {
+        "corrFilename": "Rod A 10 kHz.csv",
+        "stateFilename": "State_B3.state",
+        "coupling": "DC"
+    },
+    "B4": {
+        "corrFilename": "Rod A 100 kHz.csv",
+        "stateFilename": "State_B4.state",
+        "coupling": "DC"
+    },
+    "B5": {
+        "corrFilename": "Bilogic 100.csv",
+        "stateFilename": "State_B5.state",
+        "coupling": "AC",
+    },
+    "B6": {
+        "corrFilename": "Bilogic 300.csv",
+        "stateFilename": "State_B6.state",
+        "coupling": "AC"
+
+    },
+    "B7": {
+        "corrFilename": "Bilogic 300.csv",
+        "stateFilename": "State_B7.state",
+        "coupling": "AC"
+
+    }
+}
