da5518a154c81207ab2768dd7349a788515c7d9a
ThisTemba <tembamateke@gmail.com>
2022-08-16 09:49:05 -0400

    Add coupling to bands

--- src/bandsData.py
+++ src/bandsData.py
@@ -2,27 +2,27 @@ bands = {
     "B0": {
         "corrFilename": "Rod A 1 kHz.csv",
         "stateFilename": "State_B0.state",
-        "coupling": "DC"
+        "coupling": "DC",
     },
     "B1": {
         "corrFilename": "Rod A 10 kHz.csv",
         "stateFilename": "State_B1.state",
-        "coupling": "DC"
+        "coupling": "DC",
     },
     "B2": {
         "corrFilename": "Rod A 10 kHz.csv",
         "stateFilename": "State_B2.state",
-        "coupling": "DC"
+        "coupling": "DC",
     },
     "B3": {
         "corrFilename": "Rod A 10 kHz.csv",
         "stateFilename": "State_B3.state",
-        "coupling": "DC"
+        "coupling": "DC",
     },
     "B4": {
         "corrFilename": "Rod A 100 kHz.csv",
         "stateFilename": "State_B4.state",
-        "coupling": "DC"
+        "coupling": "DC",
     },
     "B5": {
         "corrFilename": "Bilogic 100.csv",
@@ -32,13 +32,11 @@ bands = {
     "B6": {
         "corrFilename": "Bilogic 300.csv",
         "stateFilename": "State_B6.state",
-        "coupling": "AC"
-
+        "coupling": "AC",
     },
     "B7": {
         "corrFilename": "Bilogic 300.csv",
         "stateFilename": "State_B7.state",
-        "coupling": "AC"
-
-    }
+        "coupling": "AC",
+    },
 }
