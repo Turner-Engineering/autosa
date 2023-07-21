8f80b37b62d417870ab75dbd9b4e1075c347a80d
ThisTemba <tembamateke@gmail.com>
2023-07-21 11:19:07 -0400

    Add get_folder_files func

--- src/instrument/folders.py
+++ src/instrument/folders.py
@@ -4,7 +4,7 @@ import csv
 def get_folder_info(inst, folder_path):
     folder_path = folder_path.replace("/", "\\")
 
-    # CAT? is short for Catalog and lists out the files in a folder
+    # CAT is short for Catalog and lists out the files in a folder
     resp = inst.query(f'MMEM:CAT? "{folder_path}"')
 
     # split by commas, but ignore commas inside quotes
@@ -23,3 +23,20 @@ def get_folder_info(inst, folder_path):
         filenames = [file_data.split(",")[0] for file_data in parts[2:]]
 
     return exists, empty, filenames
+
+
+def get_folder_files(inst, folder_path):
+    folder_path = folder_path.replace("/", "\\")
+
+    # CAT is short for Catalog and lists out the files in a folder
+    resp = inst.query(f'MMEM:CAT? "{folder_path}"')
+
+    # split by commas, but ignore commas inside quotes
+    parts = next(csv.reader([resp], skipinitialspace=True))
+
+    filenames = []
+    if parts[2] == "":
+        return filenames
+    else:
+        filenames = [file_data.split(",")[0] for file_data in parts[2:]]
+    return filenames
