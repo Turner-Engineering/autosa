3a65834a25bc02dccc011d94ac353b3c98cdff19
ThisTemba <tembamateke@gmail.com>
2023-08-04 18:44:40 -0400

    Remove unused returns

--- src/instrument/instrument.py
+++ src/instrument/instrument.py
@@ -62,27 +62,22 @@ def validate_filename(inst, inst_out_folder, filename):
 
 def run_start(inst):
     inst.write(":INIT:CONT ON")
-    return
 
 
 def run_stop(inst):
     inst.write(":INIT:CONT OFF")
-    return
 
 
 def run_reset(inst):
     inst.write(":INIT:REST")
-    return
 
 
 def disable_ref_level_offset(inst):
     inst.write(":DISP:WIND:TRAC:Y:RLEV:OFFS:STAT OFF")
-    return
 
 
 def release_inst(inst):
     inst.control_ren(pyvisa.constants.VI_GPIB_REN_DEASSERT_GTL)
-    return
 
 
 def get_ref_level(inst):
