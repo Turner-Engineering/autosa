445f7e8d17b45b5d9a1f8ee26a7b8ed81996c632
Medhashree Adhikari <medhashreea55@gmail.com>
2025-07-25 14:43:01 -0400

    Added datetime check

--- src/instrument/instrument.py
+++ src/instrument/instrument.py
@@ -144,6 +144,22 @@ def set_marker_to_max(inst):
     inst.write(":CALC:MARK1:FUNC:MAX")
 
 
+def compare_datetime(inst):
+    dt_fmt = "%Y-%m-%d %H:%M:%S"
+
+    # get inst date and time
+    inst_date = inst.query("SYST:DATE?").replace("\n", "").replace(",", "-")
+    inst_time = inst.query("SYST:TIME?").replace("\n", "").replace(",", ":")
+    inst_datetime = datetime.datetime.strptime(f"{inst_date} {inst_time}", dt_fmt)
+
+    local_datetime = datetime.datetime.now().replace(second=0, microsecond=0)
+    inst_datetime = inst_datetime.replace(second=0, microsecond=0)
+
+    datetime_diff = abs(inst_datetime - local_datetime)
+
+    return datetime_diff, datetime_diff.seconds < 300
+
+
 def save_trace(inst, csv_path):
     inst.write(":FORM:DATA ASC")  # make it readable
     inst.write(":CALC:MARK1:ACT")  # Activate marker
--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -3,7 +3,7 @@ import subprocess
 
 import customtkinter as ctk
 
-from instrument.instrument import release_inst
+from instrument.instrument import compare_datetime, release_inst
 from ui.get_resource_path import resource_path
 from ui.help_window import HelpWindow
 from ui.manual_mode import ManualModeFrame
@@ -36,6 +36,16 @@ class HeaderFrame(ctk.CTkFrame):
         self.label_color = parent.label_color
         self.configure(fg_color=self.frame_color, bg_color=self.frame_color)
         self.columnconfigure(0, weight=1)
+
+        self.dt_diff, self.dt_check = compare_datetime(self.inst)
+        self.dt_check_text = (
+            ""
+            if self.dt_check
+            else f"❌ Instrument and laptop datetime differ by {self.dt_diff}"
+        )
+        self.dt_check_color = "green" if self.dt_check else "red"
+        self.dt_match_var = ctk.StringVar()
+        self.dt_match_var.set(value=self.dt_check_text)
 
         self.valid_settings_label = ctk.CTkLabel(self)
         self.settings_error_var = ctk.StringVar()
@@ -101,6 +111,17 @@ class HeaderFrame(ctk.CTkFrame):
             height=20,
         ).grid(row=1, column=0, sticky="w", padx=10)
 
+        ctk.CTkLabel(
+            self,
+            textvariable=self.dt_match_var,
+            text_color=self.dt_check_color,
+            justify="left",
+            anchor="w",
+            fg_color=self.label_color,
+            font=("", 12),
+            height=20,
+        ).grid(row=2, column=0, sticky="w", padx=10)
+
         self.valid_settings_label = ctk.CTkLabel(
             self,
             textvariable=self.settings_error_var,
@@ -120,7 +141,11 @@ class HeaderFrame(ctk.CTkFrame):
     def update_valid(self):
         is_valid = self.is_valid_settings()
         self.settings_error_var.set(
-            value=("" if is_valid else "❌ Settings Invalid. Please change settings.")
+            value=(
+                ""
+                if is_valid
+                else "❌ Settings Invalid. Please change settings.                "
+            )
         )
         self.settings_error_color = "green" if is_valid else "red"
         self.valid_settings_label.configure(text_color=self.settings_error_color)
